29a2560560551062fc2073944efde7a257705e20
ldhnet <574427343@qq.com>
2022-06-28 19:03:57 +0800

    code review

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,6 @@
 import { get } from '@/axios'
+import axios from 'axios';
+
 const BASE_URL = process.env.BASE_URL
 // 获取组织机构根节点
 export const GET_DEPT_ROOT = () => get( BASE_URL + 'depRoot.json' )
@@ -14,3 +16,7 @@ export const GET_USER_BY_DEPT = data => get( BASE_URL + 'userData.json', data )
 
 // 获取Mock数据
 export const GET_MOCK_CONF = () => get(BASE_URL + 'mockConf.json')
+// 获取Mock数据
+export const GET_MOCK_CONF2 = () => {
+    axios.post("http://localhost:9010/Test/PostTest?Id=1"); 
+}
